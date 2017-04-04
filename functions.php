b1c316b4634121ae2b13c50d1a1981883d8429b0
DevCon3 <hinglejuice.gmail.com>
2017-04-03 23:02:45 -0500

    Removed Wordpress Custom Header and Background Functionality

--- functions.php
+++ functions.php
@@ -59,12 +59,6 @@ function sidestrap_setup() {
 		'caption',
 	) );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'sidestrap_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
-
 	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
 }
@@ -116,11 +110,6 @@ function sidestrap_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'sidestrap_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
