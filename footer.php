a319b430d0f36eca15642dc866890a1ee47db827
DevCon3 <hinglejuice.gmail.com>
2017-04-03 21:47:43 -0500

    Initial commit - uploaded Underscores Starter Theme

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package SideStrap
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sidestrap' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'sidestrap' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'sidestrap' ), 'sidestrap', '<a href="https://automattic.com/" rel="designer">Christian Hingle</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
