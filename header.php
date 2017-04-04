787edc800978705d59ccdef995e67e8a043239d2
DevCon3 <hinglejuice.gmail.com>
2017-04-04 09:56:46 -0500

    Added Custom Block Color Functionality

--- header.php
+++ header.php
@@ -27,14 +27,9 @@
 	<div id="right-block"></div>
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="h1background"><?php bloginfo( 'name' ); ?></h1>
 			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
 			$description = get_bloginfo( 'description', 'display' );
 			if ( $description || is_customize_preview() ) : ?>
 				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
--- inc/customizer.php
+++ inc/customizer.php
@@ -14,8 +14,77 @@ function sidestrap_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	/**
+	* Top Block Custom Color*
+	*/
+	$wp_customize->add_setting( 'sidestrap_top_block_color', array(
+		'default'    => '#455A64',
+	  'transport'  => 'refresh'
+	));
+
+	$wp_customize->add_section( 'sidestrap_custom_block_colors', array(
+		'title'    => __('Block Colors', 'SideStrap'),
+	  'priority'  => 30
+	));
+
+	$wp_customize->add_control( new WP_Customize_Color_Control($wp_customize,'sidestrap_top_block_control', array(
+		'label'    => __('Top Block Color', 'SideStrap'),
+	  'section'  => 'sidestrap_custom_block_colors',
+		'settings' => 'sidestrap_top_block_color'
+	)));
+
+	/**
+	* Left Block Custom Color*
+	*/
+	$wp_customize->add_setting( 'sidestrap_left_block_color', array(
+		'default'    => '#263238',
+	  'transport'  => 'refresh'
+	));
+
+	$wp_customize->add_control( new WP_Customize_Color_Control($wp_customize,'sidestrap_left_block_control', array(
+		'label'    => __('Left Block Color', 'SideStrap'),
+	  'section'  => 'sidestrap_custom_block_colors',
+		'settings' => 'sidestrap_left_block_color'
+	)));
+
+	/**
+	* Right Block Custom Color*
+	*/
+	$wp_customize->add_setting( 'sidestrap_right_block_color', array(
+		'default'    => '#263238',
+		'transport'  => 'refresh'
+	));
+
+	$wp_customize->add_control( new WP_Customize_Color_Control($wp_customize,'sidestrap_right_block_control', array(
+		'label'    => __('Right Block Color', 'SideStrap'),
+		'section'  => 'sidestrap_custom_block_colors',
+		'settings' => 'sidestrap_right_block_color'
+	)));
 }
+
 add_action( 'customize_register', 'sidestrap_customize_register' );
+
+/**
+* Outputs Customizer CSS changes*
+*/
+function sidestrap_customizer_css_changes() { ?>
+	<style type="text/css">
+    #top-block {
+			background: <?php echo get_theme_mod('sidestrap_top_block_color'); ?>;
+		}
+		#left-block,
+		#left-block::before {
+			background: <?php echo get_theme_mod('sidestrap_left_block_color'); ?>;
+		}
+		#right-block,
+		#right-block::before {
+			background: <?php echo get_theme_mod('sidestrap_right_block_color'); ?>;
+		}
+	</style>
+<?php }
+
+add_action('wp_head', 'sidestrap_customizer_css_changes');
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
